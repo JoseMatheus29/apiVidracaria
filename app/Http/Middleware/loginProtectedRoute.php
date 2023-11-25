e08c0b47394a29689d2e9bd1ef4983ae07a087fb
JoseMatheus29 <mateus.alvino.101@gmail.com>
2023-11-25 14:49:34 -0300

    criação do controlado de budget e concestando midl

--- app/Http/Middleware/loginProtectedRoute.php
+++ app/Http/Middleware/loginProtectedRoute.php
@@ -12,24 +12,19 @@ use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class loginProtectedRoute extends BaseMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next)
     {
         try{
             $user = JWTAuth::parseToken()->authenticate();
         }catch(\Exception $erro){
             if($erro instanceof TokenInvalidException){
-                return new Response('Token invalido'. "<br>"  . $erro);
+                return response(['Token invalido'],403);
             }
            else if($erro instanceof TokenExpiredException){
-                return new Response('Token expirado' . "<br>"  . $erro);
-            }
+            return response(['Token expirado'],400);
+        }
             else{
-                return new Response('Token de autorização não encontrado' . "<br>"  . $erro);
+                return response(['Token não encotrado'],401);
             }
         }
         return $next($request);

4d7b1ac0f8ab9d859d900a82be348c85eecdfec3
JoseMatheus29 <mateus.alvino.101@gmail.com>
2023-11-25 17:14:58 -0300

    create controller products

--- app/Http/Controllers/api/productController.php
+++ app/Http/Controllers/api/productController.php
@@ -4,8 +4,49 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\product;
 
 class productController extends Controller
 {
-    //
+    public function registerProduct(Request $request){
+        try{
+            $product = new product();
+            $product->name = $request->name;
+            $product->category = $request->category;
+            $product->sheets = $request->sheets;
+            $product->save();
+            return ['status' => 'ok'];
+
+        }catch(\Exception $erro){
+            return ['status' => 'erro', 'details' => $erro];
+        }
+    }
+    public function listAllProducts()  {
+        try{
+            $product = product::all();
+            return $product;
+        }catch(\Exception $erro){
+            return ['status' => 'erro', 'details' => $erro];
+        }
+    }
+    public function listProduct( $id){
+        try{
+            $product = product::find ($id);
+            return $product;
+
+        }catch(\Exception $erro){
+            return ['status' => 'erro', 'details' => $erro];
+        }
+    }
+
+    public function deleteProduct($id){
+        try{
+            $product = product::find($id);
+            $product->delete();
+            return ['status' => 'ok'];
+
+        }catch(\Exception $erro){
+            return ['status' => 'erro', 'details' => $erro];
+        }
+    }
 }

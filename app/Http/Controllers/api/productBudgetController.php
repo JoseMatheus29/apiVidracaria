94dd8bfe6180736d8d9fd4bc2bc76808766401ce
JoseMatheus29 <mateus.alvino.101@gmail.com>
2023-11-25 17:27:19 -0300

    fix bug product controller

--- app/Http/Controllers/api/productBudgetController.php
+++ app/Http/Controllers/api/productBudgetController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\productBudget;
 
-class productController extends Controller
+class productBudgetController extends Controller
 {
     public function registerProduct(Request $request){
         try{
